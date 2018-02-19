035ef7e784d9cc4572b20d57c3a545d8be3693ef
mikee03 <36612334+mikee03@users.noreply.github.com>
2018-02-19 13:04:35 +0800

    Add files via upload

---
+++ Exercise 1.py
@@ -0,0 +1,5 @@
+money=input("How much is your money?")
+quantity=money/15000
+print("You can buy", quantity, "of iphone")
+kulang=(quantity+1)*15000-money
+print("You will need", kulang, "to buy an additional phone.")
---
+++ Exercise 2.py
@@ -0,0 +1,5 @@
+factorial=input("Hello\nPlease input a number:")
+ans=1
+for i in range(1,factorial+1,+1):
+     ans=(ans*i)
+print(factorial, "factorial is", ans)
